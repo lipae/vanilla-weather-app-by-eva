7560e7136d508688361ea94f7bd53a814e973570
lipae <eva.v.p@gmx.net>
2022-01-12 14:44:15 +0100

    function showing weather in current location

--- src/html.js
+++ src/html.js
@@ -1,11 +1,9 @@
 function sanDiegoWeather(response) {
-  console.log(response.data);
   document.querySelector("#degrees").innerHTML = Math.round(
     response.data.main.temp
   );
   document.querySelector("#weatherDescription").innerHTML =
     response.data.weather[0].description;
-
   document.querySelector("#windElement").innerHTML = Math.round(
     response.data.wind.speed
   );
@@ -17,7 +15,31 @@ apiKey = "2ef21ee4568e04db5d3af37dfef78d7b";
 apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(sanDiegoWeather);
 
-// function showPosition() {
-//   navigator.geolocation.getCurrentPosition(displayCity);
-// }
-// <input type="button" value="Add Map" onclick="showPosition()" />;
+function showCurrentLocationWeather(response) {
+  document.querySelector(".city-name").innerHTML = response.data.name;
+  document.querySelector("#degrees").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weatherDescription").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#windElement").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidityElement").innerHTML =
+    response.data.main.humidity;
+}
+
+function handlePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  apiKey = "2ef21ee4568e04db5d3af37dfef78d7b";
+  apiUrlLatLon = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrlLatLon).then(showCurrentLocationWeather);
+}
+
+function fetchLocation() {
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+fetchLocation();
+// let locationButton = document.querySelector("#location-button");
+// locationButton.addEventListener("click", fetchLocation);
