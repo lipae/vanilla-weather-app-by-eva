178d184162673aab8f7d9b841332444e72128cd5
lipae <eva.v.p@gmx.net>
2022-01-12 13:13:28 +0100

    js function showing weather in default city (san francisco)

--- src/html.js
+++ src/html.js
@@ -1,1 +1,23 @@
+function sanDiegoWeather(response) {
+  console.log(response.data);
+  document.querySelector("#degrees").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weatherDescription").innerHTML =
+    response.data.weather[0].description;
 
+  document.querySelector("#windElement").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidityElement").innerHTML =
+    response.data.main.humidity;
+}
+city = "San Diego";
+apiKey = "2ef21ee4568e04db5d3af37dfef78d7b";
+apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(sanDiegoWeather);
+
+// function showPosition() {
+//   navigator.geolocation.getCurrentPosition(displayCity);
+// }
+// <input type="button" value="Add Map" onclick="showPosition()" />;
